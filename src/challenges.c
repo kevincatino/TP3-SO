8cc0c27bf1413a00736684e7399eefcb6e9d2d5e
khcatino <kcatino@itba.edu.ar>
2022-06-07 12:40:26 -0300

    Challenge 10 implementado

--- src/challenges.c
+++ src/challenges.c
@@ -17,8 +17,9 @@ static int challenge6();
 static int challenge7();
 static int challenge8();
 static int challenge9();
+static int challenge10();
 
-int (*challenges[CHALLENGE_QTY])(void) = {&challenge1, &challenge2, &challenge3, &challenge4, &challenge5, &challenge6, &challenge7, &challenge8, &challenge9};
+int (*challenges[CHALLENGE_QTY])(void) = {&challenge1, &challenge2, &challenge3, &challenge4, &challenge5, &challenge6, &challenge7, &challenge8, &challenge9, &challenge10};
 
 int clientFd;
 
@@ -52,7 +53,6 @@ void challengesLoop(int fd)
     int challenge = 0;
     while (challenge < CHALLENGE_QTY)
     {
-        printf("HOLA\n");
         system("clear");
         printf("%s", DESAFIO_MSJ);
         if (challenges[challenge]() == 0)
@@ -72,13 +72,13 @@ static int challenge1()
 {
     printf(
         "%s", "Bienvenidos al TP3 y felicitaciones, ya resolvieron el primer acertijo.\n\n"
-        "En este TP deberán finalizar el juego que ya comenzaron resolviendo los desafíos de cada nivel.\n"
-        "Además tendrán que investigar otras preguntas para responder durante la defensa.\n"
-        "El desafío final consiste en crear un programa que se comporte igual que yo, es decir, que provea los mismos desafíos"
-        " y que sea necesario hacer lo mismo para resolverlos. No basta con esperar la respuesta."
-        "Además, deberán implementar otro programa para comunicarse conmigo.\n\n"
-        "Deberán estar atentos a los easter eggs.\n\n"
-        "Para verificar que sus respuestas tienen el formato correcto respondan a este desafío con la palabra 'entendido\\n'\n\n");
+              "En este TP deberán finalizar el juego que ya comenzaron resolviendo los desafíos de cada nivel.\n"
+              "Además tendrán que investigar otras preguntas para responder durante la defensa.\n"
+              "El desafío final consiste en crear un programa que se comporte igual que yo, es decir, que provea los mismos desafíos"
+              " y que sea necesario hacer lo mismo para resolverlos. No basta con esperar la respuesta."
+              "Además, deberán implementar otro programa para comunicarse conmigo.\n\n"
+              "Deberán estar atentos a los easter eggs.\n\n"
+              "Para verificar que sus respuestas tienen el formato correcto respondan a este desafío con la palabra 'entendido\\n'\n\n");
     printf("%s", INVESTIGAR_MSJ);
     printf("%s", "¿Cómo descubrieron el protocolo, la dirección y el puerto para conectarse?\n\n");
 
@@ -90,7 +90,7 @@ static int challenge2()
 
     printf(
         "%s", "The Wire S1E5\n"
-        "5295 888 6288\n\n\n");
+              "5295 888 6288\n\n\n");
     printf("%s", INVESTIGAR_MSJ);
     printf("%s", "¿Qué diferencias hay entre TCP y UDP y en qué casos conviene usar cada uno?\n\n");
 
@@ -109,9 +109,9 @@ static int challenge3()
 static int challenge4()
 {
     printf("%s", "EBADF...\n\n");
-    char* respuestaCorrecta = "La respuesta correcta es fk3wfLCm3QvS";
+    char *respuestaCorrecta = "La respuesta correcta es fk3wfLCm3QvS";
 
-     if (write(13, respuestaCorrecta, strlen(respuestaCorrecta)) == -1)
+    if (write(13, respuestaCorrecta, strlen(respuestaCorrecta)) == -1)
         perror("write");
 
     printf("\n");
@@ -121,12 +121,12 @@ static int challenge4()
     return verifyAnswer("fk3wfLCm3QvS");
 }
 
-static int challenge5() 
+static int challenge5()
 {
     printf("%s", "respuesta = strings:277\n\n");
     printf("%s", INVESTIGAR_MSJ);
     printf("%s", "¿Cómo garantiza TCP que los paquetes llegan en orden y no se pierden?\n");
-    
+
     return verifyAnswer("too_easy");
 }
 
@@ -139,45 +139,75 @@ static int challenge6()
     return verifyAnswer(".RUN_ME");
 }
 
-static int challenge7() {
-  printf("Filter error\n\n");
-  char *s = "La respuesta es K5n2UFfpFMUN\n";
-  setvbuf(stdout, NULL, _IONBF, 0);
-// There are 95 printable ASCII characters, numbered 32 to 126.
-  for (int i = 0; i < strlen(s); i++) {
-    for (int j = 0; j < rand() % 30; j++) {
-      char c = ((rand() % 95) + 32);
-      write(2, &c, 1);
+static int challenge7()
+{
+    printf("Filter error\n\n");
+    char *s = "La respuesta es K5n2UFfpFMUN\n";
+    setvbuf(stdout, NULL, _IONBF, 0);
+    // There are 95 printable ASCII characters, numbered 32 to 126.
+    for (int i = 0; i < strlen(s); i++)
+    {
+        for (int j = 0; j < rand() % 30; j++)
+        {
+            char c = ((rand() % 95) + 32);
+            write(2, &c, 1);
+        }
+        // scanf("%d", &c);
+        write(1, &s[i], 1);
+        // setvbuf(stdout, NULL, _IONBF, 0);
     }
-    // scanf("%d", &c);
-    write(1, &s[i], 1);
-    // setvbuf(stdout, NULL, _IONBF, 0);
-  }
-  printf("\n");
-  printf(INVESTIGAR_MSJ);
-  printf("¿Cómo se puede implementar un servidor que atienda muchas conexiones sin usar procesos ni threads?\n\n");
+    printf("\n");
+    printf(INVESTIGAR_MSJ);
+    printf("¿Cómo se puede implementar un servidor que atienda muchas conexiones sin usar procesos ni threads?\n\n");
 
-  return verifyAnswer("K5n2UFfpFMUN");
+    return verifyAnswer("K5n2UFfpFMUN");
 }
 
-static int challenge8() {
-  printf("¿?\n\n");
-  printf("\033[30;40mLa respuesta es BUmyYq5XxXGt\033[0m\n\n");
-  printf("%s", INVESTIGAR_MSJ);
-  printf("¿Qué aplicaciones se pueden utilizar para ver el tráfico por la red?\n\n");
+static int challenge8()
+{
+    printf("¿?\n\n");
+    printf("\033[30;40mLa respuesta es BUmyYq5XxXGt\033[0m\n\n");
+    printf("%s", INVESTIGAR_MSJ);
+    printf("¿Qué aplicaciones se pueden utilizar para ver el tráfico por la red?\n\n");
 
-  return verifyAnswer("BUmyYq5XxXGt");
+    return verifyAnswer("BUmyYq5XxXGt");
 }
 
-static int challenge9() {
-  printf(
-    "Latexme\n\n"
-    "Si\n"
-    " \\mathrm{d}y = u^v{\\cdot}(v'{\\cdot}\\ln{(u)}+v{\\cdot}\\frac{u'}{u})\n"
-    "entonces\n"
-    "y = \n\n");
-  printf("%s", INVESTIGAR_MSJ);
-  printf("sockets es un mecanismo de IPC. ¿Qué es más eficiente entre sockets y pipes?\n\n");
+static int challenge9()
+{
+    printf(
+        "Latexme\n\n"
+        "Si\n"
+        " \\mathrm{d}y = u^v{\\cdot}(v'{\\cdot}\\ln{(u)}+v{\\cdot}\\frac{u'}{u})\n"
+        "entonces\n"
+        "y = \n\n");
+    printf("%s", INVESTIGAR_MSJ);
+    printf("sockets es un mecanismo de IPC. ¿Qué es más eficiente entre sockets y pipes?\n\n");
 
-  return verifyAnswer("u^v");
+    return verifyAnswer("u^v");
+}
+
+static int challenge10()
+{
+    printf("quine\n\n");
+
+    if (system("gcc quine.c -o quine.out") != 0)
+        printf("\nENTER para reintentar.\n\n");
+    else
+    {
+        printf("¡Genial!, ya lograron meter un programa en quine.c, veamos si hace lo que corresponde.\n");
+
+        if (system("./quine.out | diff quine.c -") != 0)
+        {
+            printf("\n%s\n", "diff encontró diferencias.\n");
+            printf("ENTER para reintentar.\n\n");
+        }
+        else
+            printf("La respuesta es chin_chu_lan_cha\n\n");
+    }
+
+    printf(INVESTIGAR_MSJ);
+    printf("¿Cuáles son las características del protocolo SCTP?\n\n");
+
+    return verifyAnswer("chin_chu_lan_cha");
 }
